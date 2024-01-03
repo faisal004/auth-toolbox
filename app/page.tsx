3169805b5644ea8eca4d80ec1e66dbb16595f290
FAISAL HUSAIN <88244542+faisal004@users.noreply.github.com>
2024-01-03 18:03:36 +0530

    added : home page done

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,21 @@
+import { LoginButton } from '@/components/auth/login-button'
 import { Button } from '@/components/ui/button'
-import Image from 'next/image'
-
 export default function Home() {
   return (
-    <div>
-      <Button>Hello</Button>
-    </div>
+    <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-green-500 to-green-900">
+      <div className="space-y-6 text-center">
+        <h1 className="text-6xl font-semibold text-white drop-shadow-md">
+          🔒Auth
+        </h1>
+        <p className="text-white text-lg">A simple authentication service</p>
+      </div>
+      <div className="p-4">
+        <LoginButton>
+          <Button variant="secondary" size="lg">
+            Sign in
+          </Button>
+        </LoginButton>
+      </div>
+    </main>
   )
 }
