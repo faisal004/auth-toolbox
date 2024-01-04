07e598857d28e3a3d57cfc91919617d2534413a9
FAISAL HUSAIN <88244542+faisal004@users.noreply.github.com>
2024-01-04 10:42:07 +0530

    fix : typo fixed

--- components/auth/social.tsx
+++ components/auth/social.tsx
@@ -7,10 +7,10 @@ export const Social = () => {
   return (
     <div className="flex items-center w-full gap-x-2">
       <Button size="lg" className="w-full" variant="outline" onClick={() => {}}>
-        <FcGoogle classname="h-5 w-5" />
+        <FcGoogle className="h-5 w-5" />
       </Button>
       <Button size="lg" className="w-full" variant="outline" onClick={() => {}}>
-        <FaGithub classname="h-5 w-5" />
+        <FaGithub className="h-5 w-5" />
       </Button>
     </div>
   )
