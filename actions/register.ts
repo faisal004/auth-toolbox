b4d35d4e08e7bc2a14f7d12c2cd997efa88043e3
FAISAL HUSAIN <88244542+faisal004@users.noreply.github.com>
2024-01-05 11:15:55 +0530

    added : register server action added

--- actions/register.ts
+++ actions/register.ts
@@ -2,12 +2,27 @@
 
 import { z } from "zod";
 import { RegisterSchema } from "@/schema";
-
-export const login = async (values: z.infer<typeof RegisterSchema >) => {
-    const validateFields = RegisterSchema .safeParse(values);
+import bcrypt from "bcrypt"
+import { db } from "@/lib/db";
+import { getUserByEmail } from "@/service/user";
+export const register = async (values: z.infer<typeof RegisterSchema>) => {
+    const validateFields = RegisterSchema.safeParse(values);
     if (!validateFields.success) {
         return { error: "Invalid fields" }
     }
+    const { email, name, password } = validateFields.data
+    const hashedPassword = await bcrypt.hash(password, 10)
+    const existingUser = await getUserByEmail(email)
+    if (existingUser) {
+        return { error: "Email already in use" }
+    }
+    await db.user.create({
+        data: {
+            name,
+            email,
+            password: hashedPassword
+        }
+    })
 
-    return { success: "Email Sent" }
+    return { success: "User created" }
 };
